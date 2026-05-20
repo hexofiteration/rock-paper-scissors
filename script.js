ef6a2203fe5b16b40f11f8a4e91dcdedd119c17a
hexofiteration <hexofiteration@gmail.com>
2026-05-20 19:06:30 +0200

    Add playRound() to handle round and score tracking
    Compares human and computer choices, returns a result message,
    and increments humanScore or computerScore accordingly.

--- script.js
+++ script.js
@@ -8,13 +8,59 @@ function getComputerChoice() {
     } else {
         choice = 'scissors';
     }
+    console.log(`Computer chose: ${choice}`)
     return choice;
 }
-console.log(getComputerChoice());
+
 
 function getHumanChoice() {
     let choice = prompt("Choose rock, paper or scissors");
-
-    return choice;
+    console.log(`You chose: ${choice}`)
+    return choice.toLowerCase();
 }
-console.log(getHumanChoice());
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    let message = '';
+
+    if(humanChoice === computerChoice){
+            return 'It\'s a draw!';
+        } 
+
+    switch(computerChoice) {
+    case 'rock':
+        if (humanChoice === 'scissors') {
+            message = 'You lose! Rock breaks Scissors';
+            computerScore++;
+        } else if (humanChoice === 'paper') {
+            message = 'You Win! Paper covers Rock';
+            humanScore++;
+        }
+        break;
+    case 'paper':
+        if (humanChoice === 'scissors') {
+            message = 'You Win! Scissors cut Paper';
+            humanScore++;
+        } else if (humanChoice === 'rock') {
+            message = 'You lose! Paper covers Rock';
+            computerScore++;
+        }
+        break;
+    case 'scissors':
+        if (humanChoice === 'paper') {
+            message = 'You lose! Scissors cut Paper';
+            computerScore++;
+        } else if (humanChoice === 'rock') {
+            message = 'You Win! Rock breaks Scissors';
+            humanScore++;
+        }
+        break;
+    }
+    return message;
+
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
